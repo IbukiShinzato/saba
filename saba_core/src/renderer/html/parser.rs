7724cc83d9d261a0c4d417c7b0aa6e8246457236
IbukiShinzato <e235718@ie.u-ryukyu.ac.jp>
2025-11-11 14:26:24 +0900

    bodyノードのテストまで

--- saba_core/src/renderer/html/parser.rs
+++ saba_core/src/renderer/html/parser.rs
@@ -1,10 +1,16 @@
+use crate::renderer::dom::node::Element;
+use crate::renderer::dom::node::ElementKind;
 use crate::renderer::dom::node::Node;
+use crate::renderer::dom::node::NodeKind;
 use crate::renderer::dom::node::Window;
+use crate::renderer::html::attribute::Attribute;
 use crate::renderer::html::token::HtmlToken;
 use crate::renderer::html::token::HtmlTokenizer;
 use alloc::rc::Rc;
+use alloc::string::String;
 use alloc::vec::Vec;
 use core::cell::RefCell;
+use core::str::FromStr;
 
 #[derive(Debug, Copy, Clone, PartialEq, Eq)]
 pub enum InsertionMode {
@@ -53,6 +59,176 @@ pub struct HtmlParser {
 // </html>
 
 impl HtmlParser {
+    pub fn new(t: HtmlTokenizer) -> Self {
+        Self {
+            window: Rc::new(RefCell::new(Window::new())),
+            mode: InsertionMode::Initial,
+            original_insertion_mode: InsertionMode::Initial,
+            stack_of_open_elements: Vec::new(),
+            t,
+        }
+    }
+
+    fn create_element(&self, tag: &str, attributes: Vec<Attribute>) -> Node {
+        Node::new(NodeKind::Element(Element::new(tag, attributes)))
+    }
+
+    fn insert_element(&mut self, tag: &str, attributes: Vec<Attribute>) {
+        let window = self.window.borrow();
+
+        // 現在の開いている要素スタックの最後のノードを取得
+        // スタックがからの場合は、ルート要素が現在参照しているノード
+        let current = match self.stack_of_open_elements.last() {
+            Some(n) => n.clone(),
+            None => window.document(),
+        };
+
+        // 新しい要素ノードを作成
+        let node = Rc::new(RefCell::new(self.create_element(tag, attributes)));
+
+        // 現在参照しているノードにすでに子要素がある場合
+        if current.borrow().first_child().is_some() {
+            // 最後の兄弟ノードを探索
+            let mut last_sibiling = current.borrow().first_child();
+            loop {
+                last_sibiling = match last_sibiling {
+                    Some(ref node) => {
+                        if node.borrow().next_sibling().is_some() {
+                            node.borrow().next_sibling()
+                        } else {
+                            break;
+                        }
+                    }
+                    None => unimplemented!("last_sibiling should be Some"),
+                }
+            }
+
+            // 新しいノードを最後の直後に挿入
+            last_sibiling
+                .unwrap()
+                .borrow_mut()
+                .set_next_sibling(Some(node.clone()));
+
+            node.borrow_mut().set_previous_sibling(Rc::downgrade(
+                &current
+                    .borrow()
+                    .first_child()
+                    .expect("failed to get a first child"),
+            ));
+        } else {
+            // 兄弟ノードが存在しない場合は新しいノードを現在参照しているノードの最初の子要素に設定する
+            current.borrow_mut().set_first_child(Some(node.clone()));
+        }
+
+        // 現在参照しているノードの最後の子ノードを新しいノードに設定する
+        current.borrow_mut().set_last_child(Rc::downgrade(&node));
+        // 新しいノードの親を現在参照しているノードに設定
+        node.borrow_mut().set_parent(Rc::downgrade(&current));
+
+        // 新しいノードを開いている要素スタックに追加
+        self.stack_of_open_elements.push(node);
+    }
+
+    // 開いている要素のスタック管理
+    fn pop_current_node(&mut self, element_kind: ElementKind) -> bool {
+        let current = match self.stack_of_open_elements.last() {
+            Some(n) => n,
+            None => return false,
+        };
+
+        if current.borrow().element_kind() == Some(element_kind) {
+            self.stack_of_open_elements.pop();
+            return true;
+        }
+
+        false
+    }
+
+    // stack_of_open_elementスタックから特定の種類の要素が現れるまでノードを取り出し続ける
+    fn pop_until(&mut self, element_kind: ElementKind) {
+        assert!(
+            self.contain_in_stack(element_kind),
+            "stack doesn't have an element {:?}",
+            element_kind
+        );
+
+        loop {
+            let current = match self.stack_of_open_elements.pop() {
+                Some(n) => n,
+                None => return,
+            };
+
+            if current.borrow().element_kind() == Some(element_kind) {
+                return;
+            }
+        }
+    }
+
+    // stack_of_open_elements スタックに存在する全ての要素を確認して、特定の種類の要素がある場合にtrueを返す
+    fn contain_in_stack(&mut self, element_kind: ElementKind) -> bool {
+        let contain = self
+            .stack_of_open_elements
+            .iter()
+            .any(|element| element.borrow().element_kind() == Some(element_kind));
+
+        contain
+    }
+
+    // 新しいテキストノードを作成
+    fn create_char(&self, c: char) -> Node {
+        let s = String::from(c);
+        Node::new(NodeKind::Text(s))
+    }
+
+    fn insert_char(&mut self, c: char) {
+        let current = match self.stack_of_open_elements.last() {
+            Some(n) => n.clone(),
+            None => return,
+        };
+
+        // 現在参照しているノードがテキストノードの場合、そのノードに文字を追加する
+        if let NodeKind::Text(ref mut s) = current.borrow_mut().kind {
+            s.push(c);
+            return;
+        }
+
+        // 改行文字や空白文字の時はテキストノードを追加しない
+        if c == '\n' || c == ' ' {
+            return;
+        }
+
+        // 新しいテキストノードの作成
+        let node = Rc::new(RefCell::new(self.create_char(c)));
+
+        // すでに子要素がある場合
+        if current.borrow().first_child().is_some() {
+            // 新しいテキストノードをその直後に挿入
+            current
+                .borrow()
+                .first_child()
+                .unwrap()
+                .borrow_mut()
+                .set_next_sibling(Some(node.clone()));
+            node.borrow_mut().set_previous_sibling(Rc::downgrade(
+                &current
+                    .borrow()
+                    .first_child()
+                    .expect("failed to get a first child"),
+            ));
+        } else {
+            // 新しいテキストノードを現在参照しているノードの最初の子要素として設定する
+            current.borrow_mut().set_first_child(Some(node.clone()));
+        }
+
+        // 現在参照しているノードの最後の子ノードを新しいノードに設定
+        current.borrow_mut().set_last_child(Rc::downgrade(&node));
+        // 新しいノードの親を現在参照しているノードに設定
+        node.borrow_mut().set_parent(Rc::downgrade(&current));
+
+        // 新しいノードを開いている要素スタックに追加
+        self.stack_of_open_elements.push(node);
+    }
+
     pub fn construct_tree(&mut self) -> Rc<RefCell<Window>> {
         let mut token = self.t.next();
 
@@ -226,15 +402,44 @@ impl HtmlParser {
 
                 // <body>タグのコンテンツを扱う
                 // 具体的には<div>タグ、<h1>タグ、<p>タグなど
+                // Task: タグを増やす
                 InsertionMode::InBody => {
                     match token {
+                        Some(HtmlToken::StartTag {
+                            ref tag,
+                            self_closing: _,
+                            ref attributes,
+                        }) => match tag.as_str() {
+                            // Task: ここ共通化できそう
+                            "p" => {
+                                // Elementノードを作成してDOMツリーに追加
+                                self.insert_element(tag, attributes.to_vec());
+                                token = self.t.next();
+                                continue;
+                            }
+                            "h1" | "h2" => {
+                                // Elementノードを作成してDOMツリーに追加
+                                self.insert_element(tag, attributes.to_vec());
+                                token = self.t.next();
+                                continue;
+                            }
+                            "a" => {
+                                // Elementノードを作成してDOMツリーに追加
+                                self.insert_element(tag, attributes.to_vec());
+                                token = self.t.next();
+                                continue;
+                            }
+                            _ => {
+                                token = self.t.next();
+                            }
+                        },
                         Some(HtmlToken::EndTag { ref tag }) => {
                             match tag.as_str() {
                                 "body" => {
                                     self.mode = InsertionMode::AfterBody;
                                     token = self.t.next();
 
-                                    if !self.contains_in_stack(ElementKind::Body) {
+                                    if !self.contain_in_stack(ElementKind::Body) {
                                         // パースの失敗。トークンを無視する
                                         continue;
                                     }
@@ -251,15 +456,42 @@ impl HtmlParser {
                                     }
                                     continue;
                                 }
+                                // Task: ここ共通化できそう
+                                "p" => {
+                                    let element_kind = ElementKind::from_str(tag)
+                                        .expect("failed to convert string to ElementKind");
+                                    token = self.t.next();
+                                    self.pop_until(element_kind);
+                                    continue;
+                                }
+                                "h1" | "h2" => {
+                                    let element_kind = ElementKind::from_str(tag)
+                                        .expect("failed to convert string to ElementKind");
+                                    token = self.t.next();
+                                    self.pop_until(element_kind);
+                                    continue;
+                                }
+                                "a" => {
+                                    let element_kind = ElementKind::from_str(tag)
+                                        .expect("failed to convert string to ElementKind");
+                                    token = self.t.next();
+                                    self.pop_until(element_kind);
+                                    continue;
+                                }
                                 _ => {
                                     token = self.t.next();
                                 }
                             }
                         }
+                        Some(HtmlToken::Char(c)) => {
+                            // テキストノードをDOMツリーに追加
+                            self.insert_char(c);
+                            token = self.t.next();
+                            continue;
+                        }
                         Some(HtmlToken::Eof) | None => {
                             return self.window.clone();
                         }
-                        _ => {}
                     }
                 }
 
@@ -341,5 +573,79 @@ impl HtmlParser {
         }
 
         self.window.clone()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::alloc::string::ToString;
+
+    // 空文字のテスト
+    // DOMツリーのルートにNodeKind::Documentを持つ
+    #[test]
+    fn test_empty() {
+        let html = "".to_string();
+        let t = HtmlTokenizer::new(html);
+        let window = HtmlParser::new(t).construct_tree();
+        let expected = Rc::new(RefCell::new(Node::new(NodeKind::Document)));
+
+        assert_eq!(expected, window.borrow().document());
+    }
+
+    // bodyノードのテスト
+    // <thml>タグ、<head>タグ、<body>タグを含む文字列のテスト
+    // NodeKind::Document -> NodeKind::Element
+    #[test]
+    fn test_body() {
+        let html = "<html><head></head><body></body></html>".to_string();
+        let t = HtmlTokenizer::new(html);
+        let window = HtmlParser::new(t).construct_tree();
+
+        // ルートノードがNodeKind::Documentであるか
+        let document = window.borrow().document();
+        assert_eq!(
+            Rc::new(RefCell::new(Node::new(NodeKind::Document))),
+            document
+        );
+
+        // その子供のノードはhtmlのNodeKind::Elementであるか
+        let html = document
+            .borrow()
+            .first_child()
+            .expect("failed to get a first child of document");
+        assert_eq!(
+            Rc::new(RefCell::new(Node::new(NodeKind::Element(Element::new(
+                "html",
+                Vec::new()
+            ))))),
+            html
+        );
+
+        // さらにその子供のノードはheadのNodeKind::Elementであるか
+        let head = html
+            .borrow()
+            .first_child()
+            .expect("failed to get a first child of html");
+        assert_eq!(
+            Rc::new(RefCell::new(Node::new(NodeKind::Element(Element::new(
+                "head",
+                Vec::new()
+            ))))),
+            head
+        );
+
+        // そしてheadノードの兄弟ノードはbodyのNodeKind::Elementであるか
+        let body = head
+            .borrow()
+            .next_sibling()
+            .expect("failed to get a next sibling of head");
+        assert!(
+            Rc::new(RefCell::new(Node::new(NodeKind::Element(Element::new(
+                "body",
+                Vec::new()
+            ))))),
+            body
+        );
     }
 }
